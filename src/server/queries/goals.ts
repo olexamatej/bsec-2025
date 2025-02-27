5a69f3e04181fd7109e4aab3e618c6504a52bec5
Metjuas <olexa.matej@gmail.com>
2025-02-27 18:20:17 +0100

    🩹 Fixed ✨ Merge 😎

--- src/server/queries/goals.ts
+++ src/server/queries/goals.ts
@@ -9,38 +9,25 @@ export const addGoal = async (
   target: number,
   target_date: Date | null,
 ) => {
-<<<<<<< HEAD
-  return await db.insert(goals).values({
-    user_id,
-    name,
-    amount,
-    target,
-    target_date: target_date,
-  });
+  return await db
+    .insert(goals)
+    .values({
+      user_id,
+      name,
+      amount,
+      target,
+      target_date: target_date,
+    })
+    .returning();
 };
 
 export const getGoalsByUserId = async (user_id: string) => {
   return await db.query.goals.findMany({
     where: eq(goals.user_id, user_id),
+    with: {
+      checkpoints: true,
+    },
   });
-=======
-    return await db.insert(goals).values({
-        user_id,
-        name,
-        amount,
-        target,
-        target_date: target_date,
-    }).returning();
-};
-
-export const getGoalsByUserId = async (user_id: string) => {
-    return await db.query.goals.findMany({
-        where: eq(goals.user_id, user_id),
-        with: {
-            checkpoints: true,
-        },
-    });
->>>>>>> ca63fa869251370331da3cf551bb8206c4a3438b
 };
 
 export const deleteGoal = async (id: string) => {
@@ -48,29 +35,24 @@ export const deleteGoal = async (id: string) => {
 };
 
 export const getGoalById = async (id: string) => {
-<<<<<<< HEAD
   return await db.query.goals.findFirst({
     where: eq(goals.id, id),
+    with: {
+      checkpoints: true,
+    },
   });
 };
 
 export type GoalWithDeps = Awaited<ReturnType<typeof getGoalsByUserId>>[number];
-=======
-    return await db.query.goals.findFirst({
-        where: eq(goals.id, id),
-        with: {
-            checkpoints: true,
-        }
-    });
-}
 
-export type GoalWithDeps = Awaited<ReturnType<typeof getGoalsByUserId>>[number];
-
-export const addGoalCheckpoint = async (goal_id: string, interval_amount: number, interval: number) => {
-    return await db.insert(goalCheckpoints).values({
-        goal_id,
-        interval_amount,
-        interval,
-    });
+export const addGoalCheckpoint = async (
+  goal_id: string,
+  interval_amount: number,
+  interval: number,
+) => {
+  return await db.insert(goalCheckpoints).values({
+    goal_id,
+    interval_amount,
+    interval,
+  });
 };
->>>>>>> ca63fa869251370331da3cf551bb8206c4a3438b
