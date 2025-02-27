96cf8c19452744f98bb5b08c154c23d6b32cd683
TheRamsay <ramsayofbolton@gmail.com>
2025-02-27 16:26:59 +0100

    feat: Add user selection based on cookie

--- src/app/goals/page.tsx
+++ src/app/goals/page.tsx
@@ -1,8 +1,13 @@
+import { cookies } from "next/headers";
 import Meadow from "~/components/goals/meadow";
 import { getGoalsByUserId } from "~/server/queries/goals";
 
 export default async function Page() {
-  const goals = await getGoalsByUserId("c3b9cd23-1298-41a1-889c-8f7639aff150");
+  const cookieList = await cookies();
+  const userId = cookieList.get("selectedUserId")?.value;
+  const goals = await getGoalsByUserId(
+    userId ?? "c3b9cd23-1298-41a1-889c-8f7639aff150",
+  );
 
   return (
     <div className="h-full w-full items-center justify-center">
