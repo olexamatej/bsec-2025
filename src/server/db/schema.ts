410d86a766195b04fd2d408e3986bbf47cf7acd9
LufyCZ <me@lufy.cz>
2025-02-27 10:03:27 +0000

    🤯initial database schema

--- src/server/db/schema.ts
+++ src/server/db/schema.ts
@@ -1,13 +1,15 @@
 // Example model schema from the Drizzle docs
 // https://orm.drizzle.team/docs/sql-schema-declaration
 
-import { sql } from "drizzle-orm";
+import { relations, sql } from "drizzle-orm";
 import {
-  index,
+  type AnyPgColumn,
   integer,
+  pgEnum,
   pgTableCreator,
+  text,
   timestamp,
-  varchar,
+  uuid,
 } from "drizzle-orm/pg-core";
 
 /**
@@ -18,37 +20,182 @@ import {
  */
 export const createTable = pgTableCreator((name) => `bsec-2025_${name}`);
 
-export const accounts = createTable(
-  "account",
-  {
-    id: integer("id").primaryKey().generatedByDefaultAsIdentity(),
-    name: varchar("name", { length: 256 }),
-    balance: integer("balance").default(0).notNull(),
-    createdAt: timestamp("created_at", { withTimezone: true })
-      .default(sql`CURRENT_TIMESTAMP`)
-      .notNull(),
-    updatedAt: timestamp("updated_at", { withTimezone: true }).$onUpdate(
-      () => new Date()
-    ),
-  },
+export const users = createTable("users", {
+  id: uuid("id").primaryKey().defaultRandom(),
+
+  user_name: text("user_name").notNull(),
+  display_name: text("display_name").notNull(),
+  avatar_url: text("avatar_url"),
+});
+
+export const userRelations = relations(users, ({ many }) => ({
+  comments: many(comments),
+  goals: many(goals),
+  standingOrders: many(standingOrders),
+  transactions: many(transactions),
+  tags: many(tags),
+}));
+
+export const comments = createTable("comments", {
+  id: uuid("id").primaryKey().defaultRandom(),
+
+  parent_id: uuid("parent_id").references((): AnyPgColumn => comments.id),
+
+  user_id: uuid("user_id")
+    .notNull()
+    .references(() => users.id, {
+      onDelete: "cascade",
+    }),
+  content: text("content").notNull(),
+
+  createdAt: timestamp("created_at", { withTimezone: true })
+    .default(sql`CURRENT_TIMESTAMP`)
+    .notNull(),
+});
+
+export const commenRelations = relations(comments, ({ one }) => ({
+  user: one(users, {
+    fields: [comments.user_id],
+    references: [users.id],
+  }),
+  parent: one(comments, {
+    fields: [comments.parent_id],
+    references: [comments.id],
+  }),
+}));
+
+export const goals = createTable("goals", {
+  id: uuid("id").primaryKey().defaultRandom(),
+
+  user_id: uuid("user_id")
+    .notNull()
+    .references(() => users.id, {
+      onDelete: "cascade",
+    }),
+
+  name: text("name").notNull(),
+  amount: integer("amount").notNull(),
+  target_date: timestamp("timestamp", { mode: "date", withTimezone: true }),
+});
+
+export const goalRelations = relations(goals, ({ one, many }) => ({
+  user: one(users, {
+    fields: [goals.user_id],
+    references: [users.id],
+  }),
+  checkpoints: many(goalCheckpoints),
+}));
+
+export const goalCheckpoints = createTable("goal_checkpoints", {
+  id: uuid("id").primaryKey().defaultRandom(),
+
+  goal_id: uuid("goal_id")
+    .notNull()
+    .references(() => goals.id),
+
+  interval_start: timestamp("interval_start", {
+    mode: "date",
+    withTimezone: true,
+  }).notNull(),
+  interval: integer("interval_start").notNull(),
+  interval_amount: integer("interval_amount").notNull(),
+});
+
+export const goalCheckpointRelations = relations(
+  goalCheckpoints,
+  ({ one }) => ({
+    goal: one(goals, {
+      fields: [goalCheckpoints.goal_id],
+      references: [goals.id],
+    }),
+  }),
 );
 
-export type Account = typeof accounts.$inferSelect;
-export type AccountInsert = typeof accounts.$inferInsert;
+export const standingOrderType = pgEnum("standing_order_type", [
+  "incoming",
+  "outgoing",
+]);
 
-export const transactions = createTable(
-  "transaction",
-  {
-    id: integer("id").primaryKey().generatedByDefaultAsIdentity(),
-    accountId: integer("account_id").notNull().references(() => accounts.id),
-    description: varchar("description", { length: 256 }).notNull().default(""),
-    category: varchar("category", { length: 256 }).notNull().default(""),
-    amount: integer("amount").notNull(),
-    createdAt: timestamp("created_at", { withTimezone: true })
-      .default(sql`CURRENT_TIMESTAMP`)
-      .notNull(),
-  }
-);
+export const standingOrders = createTable("standing_orders", {
+  id: uuid("id").primaryKey().defaultRandom(),
 
-export type Transaction = typeof transactions.$inferSelect;
-export type TransactionInsert = typeof transactions.$inferInsert;
+  user_id: uuid("user_id")
+    .notNull()
+    .references(() => users.id, {
+      onDelete: "cascade",
+    }),
+
+  amount: integer("amount").notNull(),
+  tag_id: uuid("tag_id").references(() => tags.id),
+  interval_start: timestamp("interval_start", {
+    mode: "date",
+    withTimezone: true,
+  }).notNull(),
+  interval_end: timestamp("interval_end", {
+    mode: "date",
+    withTimezone: true,
+  }).notNull(),
+  interval: integer("interval_start").notNull(),
+  interval_amount: integer("interval_amount").notNull(),
+
+  order_type: standingOrderType("order_type").notNull(),
+});
+
+export const standingOrderRelations = relations(standingOrders, ({ one }) => ({
+  user: one(users, {
+    fields: [standingOrders.user_id],
+    references: [users.id],
+  }),
+  tag: one(tags, {
+    fields: [standingOrders.tag_id],
+    references: [tags.id],
+  }),
+}));
+
+export const transactions = createTable("transactions", {
+  id: uuid("id").primaryKey().defaultRandom(),
+
+  user_id: uuid("user_id")
+    .notNull()
+    .references(() => users.id, {
+      onDelete: "cascade",
+    }),
+
+  amount: integer("amount").notNull(),
+  tag_id: uuid("tag_id").references(() => tags.id),
+  timestamp: timestamp("timestamp", { withTimezone: true }).notNull(),
+
+  transaction_type: standingOrderType("transaction_type").notNull(),
+});
+
+export const transactionRelations = relations(transactions, ({ one }) => ({
+  user: one(users, {
+    fields: [transactions.user_id],
+    references: [users.id],
+  }),
+  tag: one(tags, {
+    fields: [transactions.tag_id],
+    references: [tags.id],
+  }),
+}));
+
+export const tags = createTable("tags", {
+  id: uuid("id").primaryKey().defaultRandom(),
+
+  user_id: uuid("user_id")
+    .notNull()
+    .references(() => users.id, {
+      onDelete: "cascade",
+    }),
+
+  name: text("name").notNull(),
+});
+
+export const tagRelations = relations(tags, ({ many, one }) => ({
+  user: one(users, {
+    fields: [tags.user_id],
+    references: [users.id],
+  }),
+  transactions: many(transactions),
+  standingOrders: many(standingOrders),
+}));
